9a19f1ef93b3849a2df4ae150d25456c72e7e229
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-09-13 18:57:55 +0100

    Commit Emp
    Commit the definition of the class Emp.

---
+++ Emp.java
@@ -0,0 +1,14 @@
+/*
+ *  Class Constructor
+ *
+ *  Difinitions of the class Emp with a static variable
+ *  bankVault and non-static variable name.
+ *
+ */
+
+class Emp
+{
+  String name;
+  
+  static int bankVault; // We want this value to be shared by all the objects class Emp.
+}
