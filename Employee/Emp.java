b19f88e8cf400f7a1430656770256b9e434553c9
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-09-13 19:30:32 +0100

    Adding getBankVaultValue
    Adding a getter static int.

--- Employee/Emp.java
+++ Employee/Emp.java
@@ -10,5 +10,19 @@ class Emp
 {
   String name;
   
+  public static final int MIN_AGE = 18;
+  static final int MAX_AGE = 70;
+  
   static int bankVault; // We want this value to be shared by all the objects class Emp.
+  
+  /*
+   *  static method getBankVaultValue returns the value static variable bankVault.
+   *
+   */
+  
+  static int getBankVaultValue()
+  {
+    return bankVault;
+  }
+  
 }
