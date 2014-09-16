2e08cda29000621bd02625524bddd8b7a3dd19d3
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-09-17 00:34:33 +0100

    Creating
    Creating the file source code.

---
+++ structure/Office.java
@@ -0,0 +1,13 @@
+package office
+
+class Office
+{
+	public static void main(String args[])
+	{
+		Employee emp1 = new Employee();
+		Employee emp2 = new Employee();
+		Employee emp3 = new Employee();
+		Employee emp4 = new Employee();
+		Employee emp5 = new Employee();
+	}
+}
